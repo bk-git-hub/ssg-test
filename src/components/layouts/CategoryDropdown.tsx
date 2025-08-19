47424014822431739198c87d8ae07bd805a64ec0
bk-git-hub <bkprivate99@gmail.com>
2025-08-19 13:07:48 +0900

    fix: fix image url

--- src/components/layouts/CategoryDropdown.tsx
+++ src/components/layouts/CategoryDropdown.tsx
@@ -13,7 +13,7 @@ const CategoryDropdown = () => {
     <div>
       <button className="flex items-center font-bold hover:cursor-pointer" onClick={handleToggle}>
         학습분야
-        <img src={"down.svg"} className={`${isOpen ? "rotate-180" : ""} transition-transform duration-300`} />
+        <img src={"/down.svg"} className={`${isOpen ? "rotate-180" : ""} transition-transform duration-300`} />
       </button>
 
       {isOpen && <div className="absolute left-0 h-[400px] w-screen bg-blue-900">학습 카테고리 표시될 부분</div>}
