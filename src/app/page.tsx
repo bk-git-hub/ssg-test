75402aa376397145456544e02ac473b5dcd806ee
bk-git-hub <bkprivate99@gmail.com>
2025-08-19 13:03:03 +0900

    temporary commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,36 @@
+import ArticleCardList from "@/components/landing/ArticleCardList";
 import CategoryCard from "@/components/landing/CategoryCard";
 import CategoryCardCarousel from "@/components/landing/CategoryCardCarousel";
 import IconTester from "@/components/landing/IconTester";
 import Header from "@/components/layouts/Header";
+import ShowAllLink from "@/components/common/ShowAllLink";
 import { IconPicker, Icon, IconName } from "@/components/ui/icon-picker";
-import { securityCategories } from "@/mocks/mockCategories";
+
 import Image from "next/image";
+import { mockArticles } from "@/mocks/mockArticles";
+import { securityCategories } from "@/mocks/mockCategories";
+import { mockProjects } from "@/mocks/mockProjectCards";
+import ProjectCard from "@/components/common/ProjectCard";
 
 export default function Home() {
   return (
-    <div className="font-pretendard w-screen bg-white text-black">
+    <div className="font-pretendard flex w-full flex-col items-center bg-white text-black">
       <Header />
-      <main className="flex flex-col gap-20">
-        <div className="mx-auto w-full max-w-[1200px] bg-gray-400 text-center text-3xl text-white sm:h-[250px] md:h-[400px] lg:h-[500px]">
-          배너 위치
+      <main className="flex w-full flex-col gap-20">
+        <div className="mx-auto w-full max-w-[1200px] px-8 text-center text-3xl text-white sm:h-[250px] md:h-[400px] lg:h-[500px]">
+          <div className="h-full w-full bg-gray-400"></div>
         </div>
 
-        <div className="flex w-full flex-col items-center gap-11 bg-neutral-100 p-10 md:p-20 lg:p-30">
+        <div className="flex w-full flex-col items-center gap-11 overflow-x-hidden bg-neutral-100 p-10 md:p-20 lg:p-30">
           <span className="text-neutral-1000 text-5xl font-bold">학습 분야 둘러보기</span>
           <CategoryCardCarousel items={securityCategories} />
         </div>
+
+        <ArticleCardList heading="최신 아티클" articles={mockArticles} />
+
+        {mockProjects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
       </main>
     </div>
   );
